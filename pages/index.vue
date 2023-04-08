8ff43905bf4ce2e8ecfabecb74d4945775976bf1
Philip <philip.alexander.hassialis@gmail.com>
2023-04-08 13:26:06 +0300

    exercise

--- pages/index.vue
+++ pages/index.vue
@@ -2,8 +2,6 @@
   <div class="">
     <h1>My first Vue / Nuxt app</h1>
     <nuxt-link to="/users"> Users </nuxt-link>
+    <nuxt-link to="/products"> Products </nuxt-link>
   </div>
 </template>
-
-<script>
-</script>
---
+++ pages/products/_pid.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="">
+    <h1>Product with id: {{ $route.params.pid }}</h1>
+  </div>
+</template>
