7a7e7d2b49c7265a6ca4daa699f580f51acc133e
Philip <philip.alexander.hassialis@gmail.com>
2023-04-08 13:41:05 +0300

    validating parameters

--- pages/users/_id/index.vue
+++ pages/users/_id/index.vue
@@ -1,5 +1,12 @@
 <template>
-  <h1>
-    A single user with id: {{ $route.params.id }}
-  </h1>
+  <h1>A single user with id: {{ $route.params.id }}</h1>
 </template>
+
+<script>
+export default {
+  validate(data) {
+    console.log(data);
+    return /^[0-9]+$/.test(data.params.id);
+  },
+};
+</script>
